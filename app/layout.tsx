885ceb934aabd24709adea0d0392f96faef62c8a
Reinhardt <56647015+Reinhardt254@users.noreply.github.com>
2024-04-19 12:38:20 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div>
            {children}
          <Footer />
+        </div>
         </body>
     </html>
   );
